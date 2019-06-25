1654444acb963a957de2bffaa9a9b8340d9c812b
Jhon Douglas <jhondouglas.87@gmail.com>
2019-06-25 16:43:03 -0400

    se a terminado de hacer la pantalla de chat

--- app/src/main/java/com/bayoex/yohablo/MessageActivity.java
+++ app/src/main/java/com/bayoex/yohablo/MessageActivity.java
@@ -8,7 +8,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bayoex.yohablo.Model.User;
 import com.bumptech.glide.Glide;
@@ -20,6 +23,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessageActivity extends AppCompatActivity {
@@ -29,6 +34,9 @@ public class MessageActivity extends AppCompatActivity {
 
     FirebaseUser fuser;
     DatabaseReference reference;
+
+    ImageButton btn_send;
+    EditText text_send;
 
     Intent intent;
 
@@ -51,10 +59,29 @@ public class MessageActivity extends AppCompatActivity {
 
         profile_image = findViewById(R.id.profile_image);
         username = findViewById(R.id.username);
+        btn_send = findViewById(R.id.btn_send);
+        text_send = findViewById(R.id.text_send);
 
         intent = getIntent();
-        String userid = intent.getStringExtra("userid");
+        final String userid = intent.getStringExtra("userid");
+
         fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = text_send.getText().toString();
+                if (!msg.equals(""))
+                {
+                    sendMessage(fuser.getUid(),userid,msg);
+                }else
+                    {
+                        Toast.makeText(MessageActivity.this,"No puedes enviar un mensaje vacío", Toast.LENGTH_SHORT).show();
+                    }
+                text_send.setText("");
+            }
+        });
+
         reference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
 
         reference.addValueEventListener(new ValueEventListener() {
@@ -76,7 +103,17 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    private void sendMessage(String sender, String receiver, String message)
+    {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", sender);
+        hashMap.put("receiver", receiver);
+        hashMap.put("message", message);
+
+        reference.child("Chats").push().setValue(hashMap);
     }
 }
