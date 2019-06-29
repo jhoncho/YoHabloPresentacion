55d2897595005111ab606079febc10b379a64802
Jhon Douglas <jhondouglas.87@gmail.com>
2019-06-29 00:43:57 -0400

    Se termino la pantalla de texto a voz

--- app/src/main/java/com/bayoex/yohablo/textoVoz.java
+++ app/src/main/java/com/bayoex/yohablo/textoVoz.java
@@ -2,12 +2,39 @@ package com.bayoex.yohablo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class textoVoz extends AppCompatActivity {
+
+    private Button hablarahoraboton;
+    private EditText editText;
+    TTSManager ttsManager= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_texto_voz);
+
+        ttsManager=new TTSManager();
+        ttsManager.init(this);
+
+        editText=findViewById(R.id.ingreso_texto);
+        hablarahoraboton=findViewById(R.id.hablar_ahora);
+
+        hablarahoraboton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String  text = editText.getText().toString();
+                ttsManager.initQueue(text);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ttsManager.shutDown();
     }
 }
